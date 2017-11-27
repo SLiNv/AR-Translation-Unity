e2c0e095110b26225313e3c7962f15666963d77b
Victor Zhu <zzy809@gmail.com>
2017-11-27 16:04:27 -0500

    Added Translation Function

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -8,6 +8,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine.UI;
+using SimpleJSON;
 
 namespace Vuforia
 {
@@ -89,7 +96,7 @@ namespace Vuforia
 				sentence += word.StringValue + " ";
 				Debug.Log (sentence);
 			}
-				
+			sentence = Translator.TranslateGoogleApisSimple (sentence, "en", "es");
 			GameObject.Find ("Canvas/spanishText/Text").GetComponent<Text> ().text = sentence;
             Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
             Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
@@ -135,4 +142,64 @@ namespace Vuforia
 
         #endregion // PRIVATE_METHODS
     }
+}
+
+
+public class Translator : MonoBehaviour {
+	public static string TranslateGoogleApisSimple(string InitialText, string fromCulture, string toCulture)
+	{
+		string textToSearch = InitialText;
+		string url = string.Format("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171109T001143Z.669c8014fd9ccf31.b194f6b13bc767f1d5219f9600ac35dad1ed40c9&text={2}&lang={0}-{1}&plain", fromCulture, toCulture, WWW.EscapeURL(textToSearch));
+		string responseText = "Not Set";
+		try
+		{
+			HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+			ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+			using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+			{
+				var encoding = ASCIIEncoding.UTF8;
+				using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
+				{
+					string text = reader.ReadToEnd();
+					//Found JSON parse on https://gist.github.com/grimmdev/979877fcdc943267e44c
+
+					var N = JSON.Parse(text);
+					string translationString = N["text"][0];
+					Debug.Log(translationString);
+					responseText = translationString;
+				}
+			}
+
+		}
+		catch (Exception ex)
+		{
+			responseText = string.Format("Translation Error with GoogleAPI: {0}\r\nUrl:{1}", ex.Message, url);
+		}
+		return responseText;
+	}
+
+	public static bool MyRemoteCertificateValidationCallback(System.Object sender,
+		X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+	{
+		bool isOk = true;
+		// If there are errors in the certificate chain,
+		// look at each error to determine the cause.
+		if (sslPolicyErrors != SslPolicyErrors.None) {
+			for (int i=0; i<chain.ChainStatus.Length; i++) {
+				if (chain.ChainStatus[i].Status == X509ChainStatusFlags.RevocationStatusUnknown) {
+					continue;
+				}
+				chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+				chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+				chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+				chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+				bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+				if (!chainIsValid) {
+					isOk = false;
+					break;
+				}
+			}
+		}
+		return isOk;
+	}
 }
